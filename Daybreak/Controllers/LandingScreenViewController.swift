c60d0e48e1df664b24c6d3017304690595779d18
Tyler <tysameshima@gmail.com>
2020-10-09 21:03:20 -0700

    Changed XCode requirement to 8+

--- Daybreak/Controllers/LandingScreenViewController.swift
+++ Daybreak/Controllers/LandingScreenViewController.swift
@@ -7,12 +7,20 @@
 
 import UIKit
 import GoogleSignIn
+import UserNotifications
 
 class LandingScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        let center = UNUserNotificationCenter.current()
+//        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+////            if !granted{
+////                let alertController = UIAlertController(title: "Denied access", message: "Looks like you denied access to notification. Please enable this in your settings or you won't receive notifications!", preferredStyle: .alert)
+////                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+////                self.present(alertController, animated: true, completion: nil)
+////            }
+//        }
         // Do any additional setup after loading the view.
         
         if GIDSignIn.sharedInstance()?.currentUser != nil{
