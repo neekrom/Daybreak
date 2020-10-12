1690ea8d2228de87c053a88a9f40211f901a229d
Angelina Wang <lina1@berkeley.edu>
2020-10-12 15:45:00 +0800

    Built out the Home Screen and Login Page

--- Daybreak/Controllers/LoginViewController.swift
+++ Daybreak/Controllers/LoginViewController.swift
@@ -8,6 +8,38 @@
 import UIKit
 import GoogleSignIn
 
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var googleSignIn: GIDSignInButton!
